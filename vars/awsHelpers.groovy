15eab931f33c4e63e95753583fdbce0c584b0a41
bharath-vsss <goku.baru@gmail.com>
2026-05-28 19:04:27 +0530

    Update chart name and improve deployment scripts

--- vars/awsHelpers.groovy
+++ vars/awsHelpers.groovy
@@ -1,52 +1,77 @@
+```groovy
 // vars/orchestrateDeployment.groovy
 
-// 1. Centralized Configuration Map (Internal Private Helper)
 def getConstants() {
     return [
         AWS_ACCOUNT_ID : "510931056289",
         REGION         : "ap-southeast-2",
         IMAGE_REPO     : "devops_repo/app",
-        CHART_NAME     : "ecommerce",
+        CHART_NAME     : "ecommerce-app",
         ECR_URL        : "510931056289.dkr.ecr.ap-southeast-2.amazonaws.com"
     ]
 }
 
-// 2. Main Entry Point
 def call(Map config = [:]) {
+
     def envs = getConstants()
     def fullImageUri = "${envs.ECR_URL}/${envs.IMAGE_REPO}"
 
     if (config.action == 'pushImage') {
+
         echo "Starting Docker push sequence for version: ${config.appVersion}"
-        
-        sh "aws ecr get-login-password --region ${envs.REGION} | docker login --username AWS --password-stdin ${envs.ECR_URL}"
+
+        sh """
+            aws ecr get-login-password --region ${envs.REGION} | \
+            docker login --username AWS --password-stdin ${envs.ECR_URL}
+        """
+
         sh """
             docker tag studentapp:${config.appVersion} ${fullImageUri}:${config.appVersion}
+
             docker push ${fullImageUri}:${config.appVersion}
         """
-        
+
     } else if (config.action == 'deployHelm') {
-        echo "Starting Helm package and remote deploy sequence for version: ${config.appVersion}"
-        
-        // Log helm into ECR OCI registry
-        sh "aws ecr get-login-password --region ${envs.REGION} | helm registry login --username AWS --password-stdin ${envs.ECR_URL}"
-        
-        // Package the chart using your local directory
-        sh "helm package ecommerce1/ --version 0.1.0 --app-version ${config.appVersion}"
-        
-        // FIX: Route the chart push to your existing ECR repository namespace ('devops_repo')
-        sh "helm push ecommerce-app-0.1.0.tgz oci://${envs.ECR_URL}/devops_repo"
-        
-        // FIX: Reference the correct OCI storage engine path during installation
+
+        echo "Starting Helm package and deploy sequence for version: ${config.appVersion}"
+
         sh """
-            helm upgrade --install ecommerce-release oci://${envs.ECR_URL}/devops_repo/ecommerce-app \
+            aws ecr get-login-password --region ${envs.REGION} | \
+            helm registry login --username AWS --password-stdin ${envs.ECR_URL}
+        """
+
+        sh """
+            helm package ecommerce1/ \
+              --version 0.1.0 \
+              --app-version ${config.appVersion}
+        """
+
+        // Push chart to SAME repo namespace
+        sh """
+            helm push ecommerce-app-0.1.0.tgz \
+            oci://${envs.ECR_URL}/devops_repo
+        """
+
+        // Pull/install from SAME repo namespace
+        sh """
+            helm upgrade --install ecommerce-release \
+              oci://${envs.ECR_URL}/devops_repo/ecommerce-app \
               --version 0.1.0 \
               --set image.repository=${envs.ECR_URL}/${envs.IMAGE_REPO} \
               --set image.tag=${config.appVersion} \
               --set imagePullSecrets[0].name=ecr-registry-secret \
               --kubeconfig ${config.kubeconfigPath}
         """
+
     } else {
-        error "Invalid action '${config.action}' passed to orchestrateDeployment step. Please pass action: 'pushImage' or action: 'deployHelm'."
+
+        error """
+        Invalid action '${config.action}'.
+        Use:
+        action: 'pushImage'
+        OR
+        action: 'deployHelm'
+        """
     }
 }
+```
